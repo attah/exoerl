ab3e3adc4282155fd9dc4dbc968baf0d3cb328a2
Anton Thomasson <antonthomasson@gmail.com>
2019-06-30 16:30:52 +0200

    Support more endpoints, multiple queries on the same socket.

--- exoerl.erl
+++ exoerl.erl
@@ -1,26 +1,35 @@
 -module(exoerl).
 -export([go/2, escape/1, unescape/1, xor_csum/1]).
 
-go(Host, What0) ->
-  % io:format("What: ~p~n", [What0]),
-  % timer:sleep(666),
+go(Host, What) when is_list(What) ->
+  % io:format("What: ~p: ~n", [What]),
+  timer:sleep(222),
   {ok, Sock} = gen_tcp:connect(Host, 26486, [binary, {packet, 0}]),
+  Vals = [get_data(Sock, W) || W <- What ],
+  % io:format("~p~n", [Val]);
+  ok = gen_tcp:close(Sock),
+  lists:zip(What,Vals);
+go(Host, What) ->
+  go(Host, [What]).
+
+get_data(Sock, What0) ->
   What = temp(What0),
   % Pkt = mk_tx_packet(<<16#ff, 16#1e, 16#c8, 16#04, 16#34, 16#03, 16#08, 16#09>>),
-  Pkt = mk_tx_packet(<<16#ff, 16#1e, 16#c8, 16#04, 16#b6, 16#03, 16#02, What>>),
+  Pkt = mk_tx_packet(<<16#ff, 16#1e, 16#c8, 16#04, 16#b6, 16#03, What/binary>>),
   ok = gen_tcp:send(Sock, Pkt),
   Res = do_recv(Sock),
-  ok = gen_tcp:close(Sock),
-  io:format("Res: ~p~n", [Res]),
+  % io:format("Res: ~p~n", [Res]),
   Val = decode_message(Res),
-  io:format("~p~n", [Val]).
+  Val.
 
 % every third "address" appears to be a float
-temp(outdoor) -> 16#0c;
-temp(radiator) -> 16#0f;
-temp(hot_water) -> 16#1b;
-temp(district_heating) -> 16#54;
-temp(Other) -> Other.
+temp(outdoor) -> <<16#02, 16#0c>>;
+temp(radiator) -> <<16#02, 16#0f>>;
+temp(hot_water) -> <<16#02, 16#1b>>;
+temp(district_heating) -> <<16#02, 16#54>>;
+temp(hot_water_regulator) -> <<16#04, 16#e4>>;
+temp(Other) when is_binary(Other) -> Other;
+temp(Other) -> <<16#02, Other>>.
 
 do_recv(_Sock) ->
   receive
